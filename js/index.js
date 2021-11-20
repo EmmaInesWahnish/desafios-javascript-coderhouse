e51c170d6825cd080e96903dcfb0bf2534b5c078
Emma Wahnish <ewahnish@gmail.com>
2021-11-20 14:55:14 -0300

    alert

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ let nombre = '';
 nombre = prompt('Ingrese su nombre por favor');
 if (!nombre || nombre == '' || nombre !== 'EMMA') {
     alert('Credenciales inválidas');
-    alert(nombre);
 } else {
     alert('Hola ' + nombre);
 }
