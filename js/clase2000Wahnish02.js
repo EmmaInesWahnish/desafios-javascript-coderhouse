3186889523423610a7a444afe762b06048d58cc1
Emma Wahnish <ewahnish@gmail.com>
2021-11-21 13:11:28 -0300

    New

--- js/clase2000Wahnish02.js
+++ js/clase2000Wahnish02.js
@@ -1,13 +1,17 @@
+// Se solicita ingresar un nombre
 let nombre = prompt("Por favor ingrese su Nombre");
+// Se valida la información ingresada
 if (!nombre || nombre == '') {
     alert("Es obligtorio ingresar un nombre no nulo");
     throw "Se ha ingresado información inválida" 
 } else {
+    // si la información ingresada es valida se solicita un numero
     elNumero = prompt(nombre + " por favor ingrese un numero entre 25 y 50");
+    // Se valida el número ingresado y se envían mensajes acordes a la información ingresada
     if (isNaN(elNumero) || Number(elNumero) < 25 || Number(elNumero) > 50) {
-        alert(nombre + " , el numero ingresado no responde a lo solocitado");
-        throw "Se ha ingresado un numero fuera de rango";
+        alert(nombre + " , el numero ingresado no responde a lo solicitado");
+        throw "Se ha ingresado un numero inválido o fuera del rango solicitado";
     } else {
-        alert(nombre + " , muchas gracias por su colaboración");
+        alert(nombre + " , muchas gracias por su colaboración, usted ingresó " + elNumero);
     }
 }
